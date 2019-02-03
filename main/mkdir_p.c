391e69eceb670d8dcc6e6280a023a3eb50900180
David Oberhollenzer <goliath@infraroot.at>
2019-02-03 13:21:22 +0100

    Fix mkdir_p for absolute directories
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- main/mkdir_p.c
+++ main/mkdir_p.c
@@ -12,17 +12,17 @@ int mkdir_p(const char *path)
 	size_t i, len;
 	char *buffer;
 
-	while (*path == '/')
+	while (path[0] == '/' && path[1] == '/')
 		++path;
 
-	if (*path == '\0')
+	if (*path == '\0' || (path[0] == '/' && path[1] == '\0'))
 		return 0;
 
 	len = strlen(path) + 1;
 	buffer = alloca(len);
 
 	for (i = 0; i < len; ++i) {
-		if (path[i] == '/' || path[i] == '\0') {
+		if (i > 0 && (path[i] == '/' || path[i] == '\0')) {
 			buffer[i] = '\0';
 
 			if (mkdir(buffer, 0755) != 0) {
